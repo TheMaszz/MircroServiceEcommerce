e1bb2675d4293d7b6e0105165a695102f2c60d1e
TheMaszz <t.tanawan.w@gmail.com>
2025-03-30 12:49:24 +0700

    custom mat theme

--- fe-ecommerce/src/app/shared/components/pagination/pagination.component.ts
+++ fe-ecommerce/src/app/shared/components/pagination/pagination.component.ts
@@ -6,7 +6,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './pagination.component.html',
-  styleUrl: './pagination.component.scss',
 })
 export class PaginationComponent {
   @Input() currentPage: number = 1;
